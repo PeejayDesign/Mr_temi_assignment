247676e192c13967a8193885a668314006e898d3
Bamigboye Oluwapelumi <bamigboyepelumi38@gmail.com>
2022-11-18 11:11:42 +0100

    last

--- Live-fetch/script/script.js
+++ Live-fetch/script/script.js
@@ -1,48 +1,26 @@
 // main.js...
 
 // 1. GET request using fetch()
-fetch("https://jsonplaceholder.typicode.com/posts")
+fetch("https://mocki.io/v1/70f835fa-38cd-4d85-80b5-018c9c6efa02")
   // Converting received data to JSON
   .then((response) => response.json())
   .then((json) => {
     
   // 2. Create a variable to store HTML table headers
-    let li = `<tr><th>id</th><th>title</th><th>body</th></tr>`;
+    let li = `<tr><th>id</th><th>Staff_name</th><th>Staff_lastname</th><th>Staff_age</th><th>Staff_role</th><th>Staff_skills</th></tr>`;
 
     // 3. Loop through each data and add a table row
     json.forEach((user) => {
       li += `<tr>
         <td>${user.id} </td>
-        <td>${user.title}</td>
-        <td>${user.body}</td>
+        <td>${user.Staff_name}</td>
+        <td>${user.Staff_lastname}</td>
+        <td>${user.Staff_age}</td>
+        <td>${user.Staff_role}</td>
+        <td>${user.Staff_skills}</td>
       </tr>`;
     });
 
     // 4. DOM Display result
     document.getElementById("users").innerHTML = li;
   });
-
-// main.js
-
-// // 5. POST request using fetch()
-// fetch("https://jsonplaceholder.typicode.com/posts", {
-//   // 6. Adding method type
-//   method: "POST",
-
-//   // 7. Adding body or contents to send JSON Stringify
-// //   body: JSON.stringify({
-// //     title: "foo",
-// //     body: "bar",
-// //     userId: 1
-// //   }),
-
-//   // 8. Adding headers to the request
-//   headers: {
-//     "Content-type": "application/json; charset=UTF-8"
-//   }
-// })
-//   // 9. Converting to JSON
-//   .then((response) => response.json())
-
-//   // 10. Displaying results to console
-//   .then((json) => console.log(json));
